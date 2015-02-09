3d4b0c9d40722970689f0f2b641d63c708f402e4
Maurizio Pirillo <maurizio.pirillo@gmail.com>
2015-02-09 10:37:32 +0000

    Changes made by Maurizio Pirillo (maurizio.pirillo@gmail.com).

--- ferrari/scripts/app/settings.js
+++ ferrari/scripts/app/settings.js
@@ -5,7 +5,7 @@
 var appSettings = {
 
     everlive: {
-        apiKey: '$EVERLIVE_API_KEY$', // Put your Backend Services API key here
+        apiKey: '$0fuA7KHEXcXOxyly$', // Put your Backend Services API key here
         scheme: 'http'
     },
 
